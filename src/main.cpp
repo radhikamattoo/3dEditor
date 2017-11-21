d550a180bcac6880b40b161cde1800e3d0760087
Radhika Mattoo <rm3485@nyu.edu>
2017-11-21 00:14:31 -0500

    added comments for questions & added travis badge

--- src/main.cpp
+++ src/main.cpp
@@ -32,13 +32,16 @@ using namespace Eigen;
 
 // VertexBufferObject wrapper
 VertexBufferObject VBO;
-
+VertexBufferObject VBO_I;
+VertexBufferObject VBO_C;
+vector<float> indices;
 // Orthographic or perspective projection?
 bool ortho = true;
 // Number of meshes existing in the scene
-int numMeshes = 1;
+int nummeshes = 1;
 // A cube has 6 faces, 2 triangles per face gives 12 triangles
-Eigen::MatrixXf Meshes(3,36);
+Eigen::MatrixXf meshes(3,36);
+Eigen::MatrixXf colors(3, 36);
 Eigen::Matrix4f viewport(4,4);
 Eigen::Matrix4f orthographic(4,4);
 Eigen::Matrix4f perspective(4,4);
@@ -51,7 +54,7 @@ Eigen::Matrix4f MVP(4,4);
 float theta = (PI/180) * 60;
 
 // near and far are hardcoded
-float n = -0.1;
+float n = -0.5;
 float f = -100.;
 // right and left
 float r;
@@ -62,95 +65,120 @@ float b;
 
 void drawUnitCube()
 {
-  // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-  // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-  Meshes <<
+  // TODO: USE VBO INDEX TO PREVENT REPEAT VERTICES
+  // // front
+  // meshes <<
   // -0.5, -0.5,  0.5,
+  //  0.5, -0.5,  0.5,
+  //  0.5,  0.5,  0.5,
+  // -0.5,  0.5,  0.5,
+  // // back
   // -0.5, -0.5, -0.5,
-  // -0.5,  0.5,  0.5, // end T1
-  //
+  //  0.5, -0.5, -0.5,
+  //  0.5,  0.5, -0.5,
   // -0.5,  0.5, -0.5,
-  // -0.5, -0.5, -0.5,
-  // -0.5,  0.5,  0.5, //end T2
-  //
-  // -0.5, -0.5, -0.5,
-  // -0.5,  0.5, -0.5,
-  //  0.5,  0.5, -0.5, //end T3
-  //
-  //  -0.5, -0.5, -0.5,
-  //   0.5, -0.5 -0.5,
-  //   0.5, 0.5, -0.5, //end T4
-  //
-  //   0.5, 0.5, -0.5,
-  //   0.5, -0.5, -0.5,
-  //   0.5, -0.5, 0.5, //end T5
-  //
-  //   0.5, 0.5, -0.5,
-  //   0.5, 0.5, 0.5,
-  //   0.5, -0.5, 0.5, //end T6
-  //
-  //   0.5, 0.5, 0.5,
-  //   9.5, -0.5, 0.5,
-  //   -0.5, 0.5, 0.5, //end T7
-  //
-  //   0.5, -0.5, 0.5,
-  //   -0.5, -0.5, 0.5,
-  //   -0.5, 0.5, 0.5, //end T8
-  //
-  //   -0.5, -0.5, 0.5,
-  //   0.5, -0.5, 0.5,
-  //   0.5, -0.5, -0.5, //end T9
-  //
-  //   -0.5, -0.5, 0.5,
-  //   0.5, -0.5, -0.5,
-  //   -0.5, -0.5, -0.5, //end T10
-  //
-  //   -0.5, 0.5, 0.5,
-  //   0.5, 0.5, 0.5,
-  //   0.5, 0.5, -0.5,  //end T11
-  //
-  //   -0.5, 0.5, 0.5,
-  //   -0.5, 0.5, -0.5,
-  //   0.5, 0.5, -0.5; //end T12
-    -0.5f,-0.5f,-0.5f,
-		-0.5f,-0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
-		 0.5f, 0.5f,-0.5f,
-		-0.5f,-0.5f,-0.5f,
-		-0.5f, 0.5f,-0.5f,
-		 0.5f,-0.5f, 0.5f,
-		-0.5f,-0.5f,-0.5f,
-		 0.5f,-0.5f,-0.5f,
-		 0.5f, 0.5f,-0.5f,
-		 0.5f,-0.5f,-0.5f,
-		-0.5f,-0.5f,-0.5f,
-		-0.5f,-0.5f,-0.5f,
-		-0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f,-0.5f,
-		 0.5f,-0.5f, 0.5f,
-		-0.5f,-0.5f, 0.5f,
-		-0.5f,-0.5f,-0.5f,
-		-0.5f, 0.5f, 0.5f,
-		-0.5f,-0.5f, 0.5f,
-		 0.5f,-0.5f, 0.5f,
-		 0.5f, 0.5f, 0.5f,
-		 0.5f,-0.5f,-0.5f,
-		 0.5f, 0.5f,-0.5f,
-		 0.5f,-0.5f,-0.5f,
-		 0.5f, 0.5f, 0.5f,
-		 0.5f,-0.5f, 0.5f,
-		 0.5f, 0.5f, 0.5f,
-		 0.5f, 0.5f,-0.5f,
-		-0.5f, 0.5f,-0.5f,
-		 0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f,-0.5f,
-		-0.5f, 0.5f, 0.5f,
-		 0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
-		 0.5f,-0.5f, 0.5f;
-  VBO.update(Meshes);
-}
+  // indices = {
+  //    // front
+  //    0, 1, 2,
+  //    2, 3, 0,
+  //    // top
+  //    1, 5, 6,
+  //    6, 2, 1,
+  //    // back
+  //    7, 6, 5,
+  //    5, 4, 7,
+  //    // bottom
+  //    4, 0, 3,
+  //    3, 7, 4,
+  //    // left
+  //    4, 5, 1,
+  //    1, 0, 4,
+  //    // right
+  //    3, 2, 6,
+  //    6, 7, 3
+  //  };
+  meshes <<
+  -0.5f,-0.5f,-0.5f,
+	-0.5f,-0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+	 0.5f, 0.5f,-0.5f,
+	-0.5f,-0.5f,-0.5f,
+	-0.5f, 0.5f,-0.5f,
+	 0.5f,-0.5f, 0.5f,
+	-0.5f,-0.5f,-0.5f,
+	 0.5f,-0.5f,-0.5f,
+	 0.5f, 0.5f,-0.5f,
+	 0.5f,-0.5f,-0.5f,
+	-0.5f,-0.5f,-0.5f,
+	-0.5f,-0.5f,-0.5f,
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f,-0.5f,
+	 0.5f,-0.5f, 0.5f,
+	-0.5f,-0.5f, 0.5f,
+	-0.5f,-0.5f,-0.5f,
+	-0.5f, 0.5f, 0.5f,
+	-0.5f,-0.5f, 0.5f,
+	 0.5f,-0.5f, 0.5f,
+	 0.5f, 0.5f, 0.5f,
+	 0.5f,-0.5f,-0.5f,
+	 0.5f, 0.5f,-0.5f,
+	 0.5f,-0.5f,-0.5f,
+	 0.5f, 0.5f, 0.5f,
+	 0.5f,-0.5f, 0.5f,
+	 0.5f, 0.5f, 0.5f,
+	 0.5f, 0.5f,-0.5f,
+	-0.5f, 0.5f,-0.5f,
+	 0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f,-0.5f,
+	-0.5f, 0.5f, 0.5f,
+	 0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+	 0.5f,-0.5f, 0.5f;
+  VBO.update(meshes);
 
+}
+void colorCube()
+{
+  colors <<
+  0.583f,  0.771f,  0.014f,
+  0.609f,  0.115f,  0.436f,
+  0.327f,  0.483f,  0.844f,
+  0.822f,  0.569f,  0.201f,
+  0.435f,  0.602f,  0.223f,
+  0.310f,  0.747f,  0.185f,
+  0.597f,  0.770f,  0.761f,
+  0.559f,  0.436f,  0.730f,
+  0.359f,  0.583f,  0.152f,
+  0.483f,  0.596f,  0.789f,
+  0.559f,  0.861f,  0.639f,
+  0.195f,  0.548f,  0.859f,
+  0.014f,  0.184f,  0.576f,
+  0.771f,  0.328f,  0.970f,
+  0.406f,  0.615f,  0.116f,
+  0.676f,  0.977f,  0.133f,
+  0.971f,  0.572f,  0.833f,
+  0.140f,  0.616f,  0.489f,
+  0.997f,  0.513f,  0.064f,
+  0.945f,  0.719f,  0.592f,
+  0.543f,  0.021f,  0.978f,
+  0.279f,  0.317f,  0.505f,
+  0.167f,  0.620f,  0.077f,
+  0.347f,  0.857f,  0.137f,
+  0.055f,  0.953f,  0.042f,
+  0.714f,  0.505f,  0.345f,
+  0.783f,  0.290f,  0.734f,
+  0.722f,  0.645f,  0.174f,
+  0.302f,  0.455f,  0.848f,
+  0.225f,  0.587f,  0.040f,
+  0.517f,  0.713f,  0.338f,
+  0.053f,  0.959f,  0.120f,
+  0.393f,  0.621f,  0.362f,
+  0.673f,  0.211f,  0.457f,
+  0.820f,  0.883f,  0.371f,
+  0.982f,  0.099f,  0.879f;
+  VBO_C.update(colors);
+
+}
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
     // Get the position of the mouse in the window
@@ -336,16 +364,10 @@ int main(void)
     0.0, 0.0, 0.0, 0.5;
     view = look * at;
 
-    // cout <<"Determinant: " <<view.determinant() << endl;
-    //
-    // cout << "First: " << look << endl;
-    // cout << "Second: " << at << endl;
-    // cout << "View: " << view << endl;
-
     //------------------------------------------
     // MODEL MATRIX
     //------------------------------------------
-    float degree = (PI/180) * 45;
+    float degree = (PI/180) * 40;
     model <<
     cos(degree),  0., sin(degree), 0,
     0.,           1.,           0, 0,
@@ -358,7 +380,21 @@ int main(void)
     // MVP = projection * view * model;
     // MVP = view * model;
     MVP = projection * view * model;
-    // TODO: Fix artifacts/problems with unit cube - vertices?
+    // QUESTIONS:
+    // TODO: Difference between orthographic and perspective projection
+    // with unit cube is ridiculous ?
+    // TODO: What should the default view be - orthographic or perspective?
+
+
+    // TODO: VBO INDEXING!!!!!! Vertices missing
+
+
+    //------------------------------------------
+    // COLOR MATRIX
+    //------------------------------------------
+    VBO_C.init();
+    colorCube();
+
 
     // Initialize the OpenGL Program
     // A program controls the OpenGL pipeline and it must contains
@@ -368,17 +404,21 @@ int main(void)
             "#version 150 core\n"
                     "in vec3 position;"
                     "uniform mat4 MVP;"
+                    "in vec3 color;"
+                    "out vec3 f_color;"
                     "void main()"
                     "{"
                     "    gl_Position = MVP * vec4(position, 0.5);"
+                    "    f_color = color;"
                     "}";
     const GLchar* fragment_shader =
             "#version 150 core\n"
+                    "in vec3 f_color;"
                     "out vec4 outColor;"
                     "uniform vec3 triangleColor;"
                     "void main()"
                     "{"
-                    "    outColor = vec4(triangleColor, 0.5);"
+                    "    outColor = vec4(f_color, 1.0);"
                     "}";
 
     // Compile the two shaders and upload the binary to the GPU
@@ -391,7 +431,7 @@ int main(void)
     // The following line connects the VBO we defined above with the position "slot"
     // in the vertex shader
     program.bindVertexAttribArray("position",VBO);
-
+    program.bindVertexAttribArray("color",VBO_C);
     // Save the current time --- it will be used to dynamically change the triangle color
     auto t_start = std::chrono::high_resolution_clock::now();
 
@@ -412,6 +452,7 @@ int main(void)
 
         // Clear the framebuffer
         glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         auto t_now = std::chrono::high_resolution_clock::now();
